23e985aeb2f93d47991afd3f0c5ee476350fedcd
Victor Turansky <victor.turansky@gmail.com>
2020-06-12 03:19:19 +0300

    Support search links

--- content.js
+++ content.js
@@ -4,26 +4,32 @@ const codeElementCount = codeElements.length
 for (let i = 0; i < codeElementCount; i++) {
   const element = codeElements[i]
   const content = element.textContent
-  const classId = getClassId(element.textContent)
-  if (!classId) {
+  const id = getId(element.textContent)
+  if (!id) {
     continue
   }
 
   element.textContent = ''
 
+  const href = isClassId(id)
+    ? `https://docs.yworks.com/yfileshtml/#/api/${id}`
+    : `https://docs.yworks.com/yfileshtml/#/search/${id}`
+
   const a = document.createElement('a')
-  a.href = `https://docs.yworks.com/yfileshtml/#/api/${classId}`
+  a.href = href
   a.textContent = content
   element.append(a)
 }
 
-function getClassId (text) {
-  const firstChar = text.charAt(0)
-  if (firstChar !== firstChar.toUpperCase()) return undefined
-  if (text === text.toUpperCase()) return undefined
-  if (text === text.toLowerCase()) return undefined
+function getId (text) {
   if (text === 'NaN' || text === 'RequireJS') return undefined
   if (text.indexOf('SVG') === 0) return undefined
 
   return text.split('.')[0]
+}
+
+function isClassId (id) {
+  const firstChar = id.charAt(0)
+  return firstChar === firstChar.toUpperCase() &&
+    id !== id.toUpperCase()
 }
